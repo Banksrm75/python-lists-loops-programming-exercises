36a07d796ae5a68c05c1fe317830fc2d4a1acda0
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-11-17 07:54:47 +0000

    2024_11_17_0154am

--- exercises/01.5-Add-item-to-list/app.py
+++ exercises/01.5-Add-item-to-list/app.py
@@ -1,5 +1,11 @@
 # Remember to import random function here
+import random
 
 my_list = [4, 5, 734, 43, 45]
 
 # The magic goes below
+for i in range(10):
+    my_num = random.randint(0, 800)
+    my_list.append(my_num)
+
+print(my_list)
--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -1,3 +1,12 @@
 my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 # Your code here
+new_list = []
+
+for i in range( len(my_list) ):
+    if (type(my_list[i]) == dict):
+        new_list.append(my_list[i])
+    elif (type(my_list[i]) == list):
+        new_list.append(my_list[i])
+
+print(new_list)
