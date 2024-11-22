f6e69e2be4ebfa140449333ae1d011e683009d55
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-11-22 02:10:42 +0000

    Learning Exeercises Complete

--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,21 +1,20 @@
-def lyrics_generator(list):
-    lyrics = []
+def lyrics_generator(input_list):
+    lyrics = ""
     count = 0
-    for i in range(len(list)):
-        if (list[i] == 0):
-            lyrics.append("Boom")
+
+    for num in input_list:
+        if num == 0:
+            lyrics += "Boom "
             count = 0
-        elif (list[i] == 1):
-            if count < 2:
-                lyrics.append("Drop the bass")
-                count += 1
-            else:
-                lyrics.append("Drop the bass !!!Break the bass!!!")
+        elif num == 1:
+            lyrics += "Drop the bass "
+            count += 1
+
+            if count == 3:
+                lyrics += "!!!Break the bass!!! "
                 count = 0
+
     return lyrics
-
-
-
 
 # Your code above, nothing to change after this line
 print(lyrics_generator([0,0,1,1,0,0,0]))
